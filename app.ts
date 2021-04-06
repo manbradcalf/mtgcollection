47dc5de3abf754d775707405c73ef791d1798f73
ben-medcalf <ben_medcalf@carmax.com>
2021-04-06 14:54:20 -0400

    fixed newline after set name and cleanup

--- app.ts
+++ app.ts
@@ -1,24 +1,11 @@
 import { readCSVObjects, writeCSV } from "https://deno.land/x/csv/mod.ts";
 
-const inputCSV = await Deno.open("./helvaultmini.csv");
-const outputCSV = await Deno.open("./example4.csv", {
+const inputCSV = await Deno.open("./helvault.csv");
+const outputCSV = await Deno.open("./output.csv", {
   write: true,
   create: true,
   truncate: false,
 });
-
-// const headers = [
-//   "collector_number",
-//   "extras",
-//   "language",
-//   "name",
-//   "oracle_id",
-//   "quantity",
-//   "scryfall_id",
-//   "set_code",
-//   "set_name",
-//   "price",
-// ];
 
 const sleep = (millis: number) => {
   return new Promise((resolve) => setTimeout(resolve, millis));
@@ -34,17 +21,24 @@ for await (const obj of readCSVObjects(inputCSV)) {
   const res = await fetch(`https://api.scryfall.com/cards/${obj.scryfall_id}`);
   const data = await res.json();
   const card = Object.values(obj);
-  card.forEach((c) => {
-    if (c == null) {
-      c == "n/a";
+  const price = data.prices.usd;
+
+  // price is the last column but is empty in the import csv
+  // populate price here
+  card[card.length - 1] = price;
+
+  // clear out null values as csv writer doesnt handle them
+  const cleanedCard = card.map(function (value) {
+    if (value == null) {
+      return "na";
+    } else {
+      return value;
     }
   });
-  const price = data.prices.usd;
-  card.push(price);
-  cards.push(card);
-}
 
-console.log(`cards are ${JSON.stringify(cards)}`);
+  console.log(`\ncard is ${JSON.stringify(cleanedCard)}`);
+  cards.push(cleanedCard);
+}
 
 inputCSV.close();
 
