594b6e21cf76f997056f7d97766f71832b809abc
ben-medcalf <ben_medcalf@carmax.com>
2021-06-02 12:12:06 -0400

    add basiccard interface? need to find a better way to handle this stuff

---
+++ BasicCard.ts
@@ -0,0 +1,14 @@
+export interface BasicCard {
+  name: string,
+  type_line: string
+  cmc: number,
+  colors: string[],
+  rarity: string,
+  power: number,
+  toughness: number,
+  oracle_text: string,
+  set_name: string,
+  color_identiy: string,
+  keywords: string[],
+  image_uris:string[]
+}
