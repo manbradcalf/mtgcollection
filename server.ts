b825da0954842ad7d00a0d0fbb2578ff47ea1909
manbradcalf <ben.medcalf@gmail.com>
2022-12-07 18:03:24 -0500

    update oak version fixes ghost error

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v11.0.0/mod.ts";
 import router from "./routes.ts";
 const app = new Application();
 const PORT = 8000;
